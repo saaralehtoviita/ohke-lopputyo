0271271e53da5515ae7c9143fb14bbd295c58e19
SaaraLehtoviita <saaralehtoviita1@gmail.com>
2026-04-27 09:36:06 +0300

    cleaned and commented on code, deleted code that is no longer in use

--- app.py
+++ app.py
@@ -3,34 +3,85 @@ import requests
 
 app = Flask(__name__)
 
-def get_all_countries():
-    url = 'https://restcountries.com/v3.1/all?fields=name,population,region'
+#muutetaan apista saatu maiden data "maa-olioiksi"
+def parse_country(country_data):
+    # OpenStreetMap korjaus
+    osm = country_data.get("maps", {}).get("openStreetMaps")
+    if osm and not osm.startswith("http"):
+        osm = "https://" + osm
+
+    flags = country_data.get("flags", {})
+    flag = flags.get("png") or flags.get("svg")
+
+    area = country_data.get("area") or 0
+    population = country_data.get("population") or 0
+
+    return {
+        "name": country_data["name"]["common"],
+        "flag": flag,
+        "population": population,
+        "region": country_data.get("region"),
+        "subregion": country_data.get("subregion"),
+        "capital": country_data.get("capital", {}),
+        "area": area,
+        "density": population / area if area else None,
+        "languages": country_data.get("languages", {}),
+        "currencies": country_data.get("currencies", {}),
+        "location": osm
+    }
+
+#haetaan kaikkien maiden tiedot ja parsitaan ne maa-olioiksi
+def fetch_all_countries():
+    url = 'https://restcountries.com/v3.1/all?fields=name,flag,population,region,capital,area,density,languages,currencies,location'
     try:
         response = requests.get(url)
         response.raise_for_status()
 
         data = response.json()
         
-        countries = []
-
-        for country in data: 
-            countries.append({
-                "name": country["name"]["common"],
-                "population": country["population"],
-                "region": country["region"]
-        })
-        return countries
+        return [parse_country(c) for c in data]
     
     except requests.exceptions.RequestException as e:
         print("API error: ", e)
         return []
+
+#parametrina saadun maalistan mukaan luodaan alueet, irrotetaan alueen nimi ja lasketaan väkiluvut yhteen
+def build_region_stats(countries):
+
+    #alustetaan tyhjä dictionary
+    regions = {}
+
+    #haetaan jokaisen maan arvot 
+    for c in countries:
+        region = c["region"]
+        population = c["population"]
+        area = c.get("area", 0)
+
+        #tarkistetaan, onko region jo olemassa, jos ei lisätään se ja asetetaan alkuarvoksi 0
+        if region not in regions:
+            regions[region] = {
+                "population": 0,
+                "area": 0
+            }
+
+        #kasvatetaan yhden alueen väkilukua ja areaa
+        regions[region]["population"] += population
+        regions[region]["area"] += area
+    
+    for region in regions:
+        population = regions[region]["population"]
+        area = regions[region]["area"]
+
+        regions[region]["density"] = population / area
+
+    return regions
 
 
 
 #flask routing = decorating, binds a function to a URL
 @app.route('/')
 def countries_top10():
-    countries = get_all_countries()
+    countries = fetch_all_countries()
 
     countries = [country for country in countries if country["population"] > 0]
 
@@ -49,30 +100,30 @@ def countries_top10():
 
 @app.route('/regions')
 def all_regions():
-    countries = get_all_countries()
+    countries = fetch_all_countries()
 
-    regions = {}
-
-    for c in countries:
-        region = c["region"]
-        population = c["population"]
-
-        if region not in regions:
-            regions[region] = 0
-        
-        regions[region] += population
+    regions_data = build_region_stats(countries)
     
-    print(regions)
+    print(regions_data)
 
-    region_names = list(regions.keys())
-    region_populations = list(regions.values())
+    region_list = [
+    {
+        "name": region,
+        "population": regions_data[region]["population"],
+        "area": regions_data[region]["area"],
+        "density": regions_data[region]["density"]
+    }
+    for region in regions_data
+]
 
-    return render_template('regions.html', region_names=region_names, region_populations=region_populations)
+    print(region_list)
+
+    return render_template('regions.html', region_list=region_list)
 
 
 @app.route('/countries')
 def all_countries():
-    countries = get_all_countries()
+    countries = fetch_all_countries()
     return render_template('countries.html', countries=countries)
 
 @app.route('/countriesRegion')
@@ -86,95 +137,45 @@ def get_countries_by_region():
     response = requests.get(url)
     data = response.json()
 
-    countries_region = []
+    countries_region = [parse_country(c) for c in data]
 
-    for country in data:
-        countries_region.append({
-            "name": country["name"]["common"],
-            "population": country["population"]
-        })
-    
     def get_population(country):
         return country["population"]
     
-    countries_region.sort(key=get_population, reverse=True)
+    countries_by_population = sorted(countries_region, key=get_population, reverse=True)
 
-    return render_template('countriesRegion.html', 
-                           countries_region=countries_region, 
-                           region=region.capitalize()
+    def get_density(country):
+        return country["density"]
+    
+    countries_region_by_density = sorted(countries_region, key=get_density, reverse=True)
+
+
+
+    return render_template('countriesRegion.html',
+                           countries_region = countries_region,
+                           countries_by_population=countries_by_population,
+                           countries_region_by_density=countries_region_by_density,
+                           region=region.capitalize(), 
                            )
-
-@app.route('/countriesEurope')
-def countriesEurope():
-
-    countries = get_all_countries()
-
-
-    #empty list
-    countries_europe = []
-
-    for country in countries:
-        if country["region"] == "Europe":
-            countries_europe.append({
-                countries_europe.append(country)
-            })
-    return render_template('countriesEurope.html', countries_europe=countries_europe)
 
 @app.route('/country')
 def get_country_by_name():
     name = request.args.get("name")
-    
+
     if not name:
         return render_template('error.html', message="name is required")
 
     url = f"https://restcountries.com/v3.1/name/{name}"
+
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        print(data)
+        data = requests.get(url).json()
+        country = parse_country(data[0])
 
-        if len(data) > 0:
-            country = {
-                "name": data[0]["name"]["common"],
-                "flag": data[0]["flags"]["png"],
-                "population": data[0]["population"],
-                "languages": data[0].get("languages"),
-                "region": data[0]["region"],
-                "subregion": data[0].get("subregion"),
-                "capital": data[0].get("capital")[0],
-                "area": data[0].get("area"),
-                "currencies": data[0].get("currencies", {}),
-                "density": data[0]["population"] / data[0]["area"]
-            }
+        return render_template("country.html", country=country)
 
-            return render_template('country.html', country=country)
-        
-    except requests.exceptions.RequestException as e:
-        print("API error: ", e)
-    
-        return render_template('error.html', message="Country not found")
-    
-# testing chart.js
-@app.route("/countriesGraph")
-def graph_test():
-    data = [
-        ("01-01-2020", 1597),
-        ("02-02-2020", 1456),
-        ("03-01-2020", 1908),
-        ("04-01-2020", 896),
-        ("05-01-2020", 755),
-        ("06-01-2020", 453),
-        ("07-01-2020", 1100),
-        ("08-01-2020", 1235),
-        ("09-01-2020", 1478),
-    ]
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
-
-    return render_template("countriesGraph.html", labels=labels, values=values)
-
-    
+    except Exception as e:
+        print(e)
+        return render_template('error.html', message="Country not found")    
         
 
 if __name__ == '__main__':
